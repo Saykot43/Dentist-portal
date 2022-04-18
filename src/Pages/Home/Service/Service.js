f0773c58c47c4a7b5650753e6f5dea2e04d74292
Saykot43 <saykot.cse43@gmail.com>
2022-04-18 21:24:56 +0600

    blog page edit

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -3,20 +3,20 @@ import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Service.css'
 
-const Service = ({service}) => {
-    const { picture, price, name, description} = service;
+const Service = ({ service }) => {
+    const { picture, price, name, description } = service;
     return (
         <div>
-            <Card style={{ width: '18rem' }} className='service-card'>
+            <Card style={{ width: '18rem' }} className='service-card h-100'>
                 <Card.Img variant="top" src={picture} />
                 <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Text>
-                    {description.slice(0, 200)+'...........'}
-                </Card.Text>
-                <h1>$ {price}</h1>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {description.slice(0, 200) + '...........'}
+                    </Card.Text>
+                    <h1>$ {price}</h1>
                 </Card.Body>
-                <Link to="/checkout" className='button-link py-2'>Checkout</Link>
+                <Link to="/checkout" className='button-link py-2'>Appointment</Link>
             </Card>
         </div>
     );
