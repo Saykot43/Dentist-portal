ad5a7e5bda61dfc4331dcf33aaed3c95916cddd0
Saykot43 <saykot.cse43@gmail.com>
2022-04-18 23:00:30 +0600

    navbar responsive

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -9,39 +9,41 @@ import './Header.css';
 const Header = () => {
     const [user] = useAuthState(auth);
 
-    const handleSignout = () =>{
+    const handleSignout = () => {
         signOut(auth);
     }
     return (
-        <div>
-            <>
-            <Navbar sticky='top' className='navbar py-4 bg-gray'>
-                <Container>
-                    <Link className='logo' to="/home"><span className='text-danger'>Dantist World</span></Link>
-                        <Nav>
-                            <NavLink className={({isActive}) =>
-                            isActive ? "active-link" : "link"} to="/home">Home</NavLink>
-                            <NavLink className={({isActive}) =>
-                            isActive ? "active-link" : "link"} to="/about">About</NavLink>
-                            <NavLink className={({isActive}) =>
-                            isActive ? "active-link" : "link"} to="/blog">Blog</NavLink>
-                            {
-                                user ?
-                                <p className='btn btn-gray font-weight-bold' onClick={handleSignout}><span className='logout'>Log out</span></p>
-                                :
-                                <>
-                                    <NavLink className={({isActive}) =>
-                                    isActive ? "active-link" : "link"} to="/login">Login</NavLink>
-                                    <NavLink className={({isActive}) =>
-                                    isActive ? "active-link" : "link"} to="/register">Sign up</NavLink>
-                                </>
-                            }
-                            
-                        </Nav>
-                </Container>
-            </Navbar>
-            </>
-        </div>
+                <Navbar className='py-2 fs-4 navbar-bg' sticky='top' expand="lg">
+                    <Container>
+                        <Navbar.Brand as={Link} to="/" className='fs-3 fw-bold text-danger'>Dantist World</Navbar.Brand>
+                        {/* <Link className='logo' to="/home"><span className='text-danger'>Dantist World</span></Link> */}
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav className="ms-auto my-2 my-lg-0"
+                        style={{ maxHeight: '350px' }}
+                        navbarScroll>
+                                <NavLink className={({ isActive }) =>
+                                    isActive ? "active-link" : "link"} to="/home">Home</NavLink>
+                                <NavLink className={({ isActive }) =>
+                                    isActive ? "active-link" : "link"} to="/about">About</NavLink>
+                                <NavLink className={({ isActive }) =>
+                                    isActive ? "active-link" : "link"} to="/blog">Blog</NavLink>
+                                {
+                                    user ?
+                                        <p className='btn btn-gray font-weight-bold' onClick={handleSignout}><span className='logout'>Log out</span></p>
+                                        :
+                                        <>
+                                            <NavLink className={({ isActive }) =>
+                                                isActive ? "active-link" : "link"} to="/login">Login</NavLink>
+                                            <NavLink className={({ isActive }) =>
+                                                isActive ? "active-link" : "link"} to="/register">Sign up</NavLink>
+                                        </>
+                                }
+
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
     );
 };
 
